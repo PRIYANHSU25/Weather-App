e884a2dc3e3c25165977e3fe65257434b54cb72b
PRIYANHSU25 <priyanshu.ranjan1972@gmail.com>
2026-07-18 13:26:28 +0530

    final project

---
+++ src/App.jsx
@@ -0,0 +1,16 @@
+
+import WeatherApp from "./WeatherApp.jsx"
+
+function App() {
+  
+  return (
+
+    <>
+     <WeatherApp />
+   
+    
+    </>
+  )
+}
+
+export default App
---
+++ src/WeatherApp.jsx
@@ -0,0 +1,28 @@
+import SearchBox from "./SearchBox.jsx";
+import InfoBox from "./InfoBox.jsx";
+import { useState } from "react";
+
+export default function WeatherApp() {
+    const [WeatherInfo, setWeatherInfo] = useState({
+        city: "Delhi",
+        feelslike: 24.48,
+        temp: 25.05,
+        tempMin: 25.05,
+        tempMax: 25.05,
+        humidity: 47,
+        weather: "haze",
+    });
+
+    let updateInfo = (newInfo) => {
+        setWeatherInfo(newInfo);
+    }
+
+
+    return(
+        <div style={{ textAlign: "center"}}>
+            <h2>Weather App by Priyanshu</h2>
+            <SearchBox  updateInfo={updateInfo}/>
+            <InfoBox  info={WeatherInfo}/>
+        </div>
+    )
+}
